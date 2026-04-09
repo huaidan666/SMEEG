69bffa9da24b4662cfcd9972e4bcc9c043c25e2f
huaidan666 <63092927+huaidan666@users.noreply.github.com>
2026-04-09 16:14:21 +0800

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import torch
-print(torch.backends.cudnn.version())
-if torch.cuda.is_available():
-    # 查看 PyTorch 编译时使用的 CUDA 版本
-    print(f"CUDA version PyTorch was built with: {torch.version.cuda}")
-    # 查看当前 GPU 的名称
-    print(f"GPU Name: {torch.cuda.get_device_name(0)}")
